11049ef0a9ae5bc6abf65f1c3111727d3b3c012f
Léo Tepedelen <leo.tepedelen@etu.umontpellier.fr>
2020-02-26 13:50:16 +0100

    oui

--- Leo/Afficheur_de_tweets.py
+++ Leo/Afficheur_de_tweets.py
@@ -5,4 +5,20 @@ i=0
 
 while os.path.isfile("donnees"+str(i)+".json") :
     print("donnees"+str(i)+".json :")
+    with open("donnees.json", "r") as mj :
+        dict_ = json.load(mj)
+        for key in dict_ :
+            if (key == "statuses") :
+                for key2 in dict_[key] :
+                    for key3 in key2 :
+                        if(key3 == "user") :
+                            print(type(key3))
+                            print(key3)
+                            print(key2[key3])
+                            for key4 in key2[key3] :
+                                print(key4)
+                        if(key3 == "contributors") :
+                            print(type(key3))
+                            print(key3)
+                            print(key2[key3])
     i += 1
--- Leo/recuperateur_de_données.py
+++ Leo/recuperateur_de_données.py
@@ -13,4 +13,3 @@ while os.path.isfile("donnees"+str(i)+".json") :
 
 with open("donnees"+str(i)+".json", "w") as donnees :
     json.dump(python_tweets.search(q='le OR la OR l OR je OR tu OR il OR nous OR vous OR ils OR elle OR elles OR ça OR opinion OR oui OR non OR pourquoi OR pense OR penser OR expression OR exprimer OR exprime OR montre OR prouve OR prouver OR montrer', lang='fr', result_type='recent', count=10), donnees, sort_keys=True, indent=4)
-
