b4bc5a65ff108a61fa55029c342e5ccad5da3641
camille <camille.Carrette@etu.umontpellier.fr>
2020-03-04 16:03:33 +0100

    testhtml

--- testhtml/projet/views.py
+++ testhtml/projet/views.py
@@ -7,20 +7,18 @@ app = Flask(__name__)
 
 mondic = {"21/02/2020":"", "22/02/2020":""}
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET','POST'])
 def index():
+	map = 'templates/map5.html'
+
 	if request.method == 'POST':
 		return render_template('map5.html')
 	else:
-		return render_template('index.html', mondic=mondic)
+		return render_template('index.html', mondic=mondic , map=map)
 
 @app.route('/rapport')
 def rapport():
 	return render_template('rapport.html')
-
-@app.route('/map')
-def map():
-	return render_template('map5.html')
 
 if __name__ == "__main__":
 	app.run()
