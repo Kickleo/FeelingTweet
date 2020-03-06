44b813c5ca0e7663454c5903b9c5b6635411ce0b
camille <camille.Carrette@etu.umontpellier.fr>
2020-03-06 12:21:01 +0100

    ça avance

--- testhtml/projet/views.py
+++ testhtml/projet/views.py
@@ -5,20 +5,25 @@ from flask.views import View
 app = Flask(__name__)
 
 
-mondic = {"21/02/2020":"", "22/02/2020":""}
+with open('tweet.json','r') as tw:
+	data = tw.read()
+
+data_tw = json.loads(data)
 
 @app.route('/', methods=['GET','POST'])
 def index():
-	map = 'templates/map5.html'
-
 	if request.method == 'POST':
 		return render_template('map5.html')
 	else:
-		return render_template('index.html', mondic=mondic , map=map)
+		return render_template('index.html', mondic=data_tw)
 
 @app.route('/rapport')
 def rapport():
 	return render_template('rapport.html')
+
+@app.route('/mapFrame')
+def mapFrame():
+	return render_template('map5.html')
 
 if __name__ == "__main__":
 	app.run()
