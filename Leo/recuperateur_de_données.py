b212c58235738b31e1082ca9ea3bf8a9135e1ed0
Léo Tepedelen <leo.tepedelen@etu.umontpellier.fr>
2020-03-10 14:54:57 +0100

    mongo

--- Leo/recuperateur_de_données.py
+++ Leo/recuperateur_de_données.py
@@ -4,24 +4,16 @@ import os.path
 import sys
 import random
 import pymongo
-
-
+import re
 
 client = pymongo.MongoClient("mongodb://Admin:LesEmotions@cluster0-shard-00-00-nbixs.gcp.mongodb.net:27017,cluster0-shard-00-01-nbixs.gcp.mongodb.net:27017,cluster0-shard-00-02-nbixs.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
 db = client.tweets
-locations = ['Kitchen','Animal','State', 'Tastey', 'Big','City','Fish', 'Pizza','Goat', 'Salty','Sandwich','Lazy', 'Fun']
-text = ['LLC','Inc','Company','Corporation']
-user = ['Pizza', 'Bar Food', 'Fast Food', 'Italian', 'Mexican', 'American', 'Sushi Bar', 'Vegetarian']
-for x in range(1, 501):
-    business = {
-        'name' : names[random.randint(0, (len(names)-1))] + ' ' + names[random.randint(0, (len(names)-1))]  + ' ' + company_type[random.randint(0, (len(company_type)-1))],
-        'rating' : random.randint(1, 5),
-        'cuisine' : company_cuisine[random.randint(0, (len(company_cuisine)-1))] 
-    }
-    result=db.reviews.insert_one(business)
+col = db.Fillon
 
-print('Created {0} of 500 as {1}'.format(x,result.inserted_id))
-print('finished creating 500 business reviews')
+# nom_col = sys.argv[1]
+
+# print('Created {0} of 500 as {1}'.format(x,result.inserted_id))
+# print('finished creating 500 business reviews')
 
 
 
@@ -29,20 +21,74 @@ python_tweets = Twython("byLgF6D4jMOstVEzCfHhwWk46", "EM9xsoauCKmNthC66aFz49gkbV
 
 
 quer = ""
-i = 0
 for oui in sys.argv :
     if (oui != sys.argv[0]) :
         quer += oui + " OR " 
-while os.path.isfile("donnees"+str(i)+".json") :
-    i += 1
-    print("donnees"+str(i)+".json")
 
 quer = quer[0:-4]
 
-print(quer)
+dict_ = python_tweets.search(q=quer , lang='fr', result_type='mixed', count=10000)
 
-with open("donnees"+str(i)+".json", "w") as donnees :
-    json.dump(python_tweets.search(q=quer , lang='fr', result_type='mixed', count=10, geocode = "46.858808,2.436929,800km"), donnees, sort_keys=True, indent=4)
+i = 0
+while (i < len(dict_["statuses"])) :
+    if ("retweeted_status" in dict_["statuses"][i]) :
+        
+        if ((re.search(".*,.*", dict_["statuses"][i]["retweeted_status"]["user"]["location"]) is not None) and (col.count_documents({"_id" : dict_["statuses"][i]["retweeted_status"]["id_str"]}) == 0)) :
+            print(col.count_documents({"id_" : dict_["statuses"][i]["retweeted_status"]["id_str"]}))
+            print("\n" + dict_["statuses"][i]["retweeted_status"]["user"]["location"])
+            print(dict_["statuses"][i]["retweeted_status"]["text"])
+            print(dict_["statuses"][i]["retweeted_status"]["created_at"])
+            print("https://twitter.com/i/web/status/" + dict_["statuses"][i]["retweeted_status"]["id_str"])
+            print(dict_["statuses"][i]["retweeted_status"]["id_str"] + "\n")
+            tweet = {
+                'location' : dict_["statuses"][i]["retweeted_status"]["user"]["location"],
+                'text' : dict_["statuses"][i]["retweeted_status"]["text"],
+                'date' : dict_["statuses"][i]["retweeted_status"]["created_at"],
+                'url' : "https://twitter.com/i/web/status/" + dict_["statuses"][i]["retweeted_status"]["id_str"],
+                '_id' : dict_["statuses"][i]["retweeted_status"]["id_str"]
+            }
+            result = col.insert_one(tweet)
+            # print(i)
+            # print("CECI EST UN retweet")
+            # print(dict_["statuses"][i]["retweeted_status"]["user"]["location"])
+            # print(dict_["statuses"][i]["retweeted_status"]["text"])
+            # print("https://twitter.com/i/web/status/" + dict_["statuses"][i]["retweeted_status"]["id_str"])
+        # else :
+        #     print("Nique bien ta mère")
+        #     print(dict_["statuses"][i]["retweeted_status"]["text"])
+        #     print("https://twitter.com/i/web/status/" + dict_["statuses"][i]["retweeted_status"]["id_str"])
+        # for key in dict_["statuses"][i]["retweeted_status"] :
+        #     print(key)
+
+    else :
+        if ((re.search(".*,.*", dict_["statuses"][i]["user"]["location"]) is not None) and (col.count_documents({"_id" : dict_["statuses"][i]["id_str"]}) == 0)) :
+            print(col.count_documents({"id_" : dict_["statuses"][i]["id_str"]}))
+            print("\n" + dict_["statuses"][i]["user"]["location"])
+            print(dict_["statuses"][i]["text"])
+            print(dict_["statuses"][i]["created_at"])
+            print("https://twitter.com/i/web/status/" + dict_["statuses"][i]["id_str"])
+            print(dict_["statuses"][i]["id_str"] + "\n")
+            tweet = {
+                'location' : dict_["statuses"][i]["user"]["location"],
+                'text' : dict_["statuses"][i]["text"],
+                'date' : dict_["statuses"][i]["created_at"],
+                'url' : "https://twitter.com/i/web/status/" + dict_["statuses"][i]["id_str"],
+                '_id' : dict_["statuses"][i]["id_str"]
+            }
+            result = col.insert_one(tweet)
+            # print(i)
+            # print("CECI EST UN tweet")
+            # print(dict_["statuses"][i]["user"]["location"])
+            # print(dict_["statuses"][i]["text"])
+            # print("https://twitter.com/i/web/status/" + dict_["statuses"][i]["id_str"])
+        # else :
+        #     print("Nique bien ta mère")
+        #     print(dict_["statuses"][i]["text"])
+        #     print("https://twitter.com/i/web/status/" + dict_["statuses"][i]["id_str"])      
+        # for key in dict_["statuses"][i] :
+        #     print(key)
+
+    i = i + 1
 
 
-"""'le OR la OR l OR je OR tu OR il OR nous OR vous OR ils OR elle OR elles OR ça OR opinion OR oui OR non OR pourquoi OR pense OR penser OR expression OR exprimer OR exprime OR montre OR prouve OR prouver OR montrer'"""
+# col.rename(nom_col)
